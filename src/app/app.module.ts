cd1659d4a61dd5bb867217e5336805af6e8652b6
Stian Gaustad <stian.gaustad@no.experis.com>
2024-03-12 08:27:52 +0100

    Scaffolding - Generate and inserted cart and total price components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { HttpClientModule } from "@angular/common/http";
 import { AppComponent } from './app.component';
 import { ItemListComponent } from './item-list/item-list.component';
 import { ItemComponent } from './item/item.component';
+import { CartComponent } from './cart/cart.component';
+import { TotalPriceComponent } from './total-price/total-price.component';
 
 @NgModule({
-  declarations: [AppComponent, ItemListComponent, ItemComponent],
+  declarations: [AppComponent, ItemListComponent, ItemComponent, CartComponent, TotalPriceComponent],
   imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
